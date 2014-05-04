e9b72ebe2bd5baf8b44f0d42aa67a6a8d81c1225
tablespoon1 <bennyval@gmail.com>
2014-05-04 13:34:46 -0400

    Base Android shit

---
+++ Middleman/Middleman/src/main/java/com/team/hv/middleman/middleman/MiddleManMainActivity.java
@@ -0,0 +1,19 @@
+package com.team.hv.middleman.middleman;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class MiddleManMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_middle_man_main);
+    }
+}
