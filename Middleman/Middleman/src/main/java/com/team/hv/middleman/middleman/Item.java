5d0b0b5a265fd62e72a3eefb48e21277f5bed3d1
tablespoon1 <bennyval@gmail.com>
2014-05-16 20:07:37 -0400

    cool stuff
    yea

---
+++ Middleman/Middleman/src/main/java/com/team/hv/middleman/middleman/Item.java
@@ -0,0 +1,8 @@
+package com.team.hv.middleman.middleman;
+
+/**
+ * Created by Ben on 5/11/2014.
+ */
+public class Item {
+    //Title, price, Description, location, high price, average price, low price
+}
