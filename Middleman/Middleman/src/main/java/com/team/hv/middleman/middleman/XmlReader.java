0c253643696d10507f68f7280f1bb5f3934d0548
tablespoon1 <bennyval@gmail.com>
2014-05-16 21:14:31 -0400

    took our on create

--- Middleman/Middleman/src/main/java/com/team/hv/middleman/middleman/XmlReader.java
+++ Middleman/Middleman/src/main/java/com/team/hv/middleman/middleman/XmlReader.java
@@ -107,7 +107,7 @@ public class XmlReader extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.feed, menu);
         return true;
     }
 
