90860fd0d55ca2092d9010e5e9325c1af5a78c02
Sam Hill <samuhill@gmail.com>
2014-05-17 12:03:16 -0400

    move stddev into xmlreader

--- Middleman/Middleman/src/main/java/com/team/hv/middleman/middleman/MiddleManMainActivity.java
+++ Middleman/Middleman/src/main/java/com/team/hv/middleman/middleman/MiddleManMainActivity.java
@@ -36,55 +36,9 @@ public class MiddleManMainActivity extends Activity {
         reader.execute();
         //Intent intent = new Intent(this,XmlReader.class);
         //startActivity(intent);
-        stdDev(XmlReader.getProducts());
         //XmlReader xr = new XmlReader();
     }
 
-    //apply standard deviation to arrayList<Product>
-    //filters out products that are not what we're looking for
-    public Double stdDev(ArrayList<Product> products){
-        int numItems = products.size();
-        double price = 0;
-        double maxPrice = 0;
-        int minPrice = 99999999;
 
-        for(int i = 0; i < products.size(); i++){
-            int currentPrice = Integer.parseInt(products.get(i).price);
 
-            if(currentPrice > maxPrice){
-                maxPrice = currentPrice;
-            }
-            if(currentPrice < minPrice){
-                minPrice = currentPrice;
-            }
-            price += currentPrice;
-        }
-        //get average
-        double avgPrice = price / numItems;
-
-        Log.v("MiddleManMainActivity","Highest price: " + maxPrice);
-        Log.v("MiddleManMainActivity", "Average price: " + avgPrice);
-        Log.v("MiddleManMainActivity","Lowest price: " + minPrice);
-
-        Log.v("MiddleManMainActivity","Number of items: " + (numItems));
-
-        //get sAvg
-        double sAvgPrice = 0;
-        for(int i = 0; i < products.size(); i++){
-            sAvgPrice += (Double.parseDouble(products.get(i).price) / 30);
-        }
-
-        //calculate stdDev
-        double stddev = 0;
-        double sum = 0;
-        for(int i = 0; i < 30; i++) {
-            double difference = Double.parseDouble(products.get(i).price) - sAvgPrice;
-            sum += difference * difference/30;
-        }
-
-        stddev = Math.pow(sum, .5);
-        Log.v("MiddleManMainActivity","stdDev: " + (stddev));
-        return stddev;
-        }
-    }
 }
